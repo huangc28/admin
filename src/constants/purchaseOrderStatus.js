ae8bd9f622dcf5d3cb8a510b8be4598b7f61ccf4
Huang Chi-Han <huangchiheng@gmail.com>
2017-06-20 11:38:20 +0800

    Trying to fix file name casing problem

---
+++ src/constants/purchaseOrderStatus.js
@@ -0,0 +1,8 @@
+export const PURCHASE_ORDER_UNFULFILLED = 1
+export const PURCHASE_ORDER_FULFILLED = 2
+
+// create purchase order steps
+export const SUPPLIER_STEP = 0
+export const PRICE_STEP = 1
+export const TRACKING_STEP = 2
+export const FINISHED_STEP = 3
