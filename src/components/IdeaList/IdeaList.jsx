584fadac4f523487fe0174c40e01f25412904f04
Huang Chi-Han <huangchiheng@gmail.com>
2017-06-21 12:56:40 +0800

    progress check.

---
+++ src/components/IdeaList/IdeaList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './IdeaList.css'
+
+class IdeaList extends Component {
+  render () {
+    return (
+      <div>
+        {/* start writing some react... */}
+      </div>
+    )
+  }
+}
+
+export default IdeaList
